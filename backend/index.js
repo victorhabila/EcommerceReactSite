4098f9018f0f0d80acae53a98bd605bff701598a
victorhabila <48987601+victorhabila@users.noreply.github.com>
2023-08-18 15:16:02 +0200

    Authentication backend api

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,18 @@
 const express = require("express");
-const cors = require("cors");
+const cors = require("cors"); // Cross Origin Resource Sharing help us communicate with the frontend
+const mongoose = require("mongoose"); //importing mongo db
+const register = require("./routes/register");
+const login = require("./routes/login");
 const product = require("./product");
 
 const app = express();
+require("dotenv").config(); //configuring our dotenv
 
 app.use(express.json()); // configuring a middle ware function
 app.use(cors()); // allow us to invoke backend from react
+
+app.use("/api/register", register); //register it to our middleware
+app.use("/api/login", login); //register it to our middleware
 
 app.get("/", (req, res) => {
   res.send("Welcome our to online shop API...");
@@ -16,5 +23,14 @@ app.get("/product", (req, res) => {
 });
 
 const port = process.env.PORT || 5000;
+const uri = process.env.DB_URI;
 
 app.listen(port, console.log(`Server running on port ${port}`));
+
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Mongodb connection is successful..."))
+  .catch((err) => console.log("Mongodb connection failed...", err.message));
---
+++ backend/routes/register.js
@@ -0,0 +1,41 @@
+const bcrypt = require("bcrypt");
+const Joi = require("joi");
+const express = require("express");
+const { User } = require("../models/user");
+const genAuthToken = require("../utils/genAuthToken");
+
+const router = express.Router();
+
+router.post("/", async (req, res) => {
+  //joi help us validate our incomming data from our form body
+  const schema = Joi.object({
+    name: Joi.string().min(3).max(30).required(),
+    email: Joi.string().min(3).max(200).required().email(),
+    password: Joi.string().min(6).max(200).required(),
+  });
+
+  const { error } = schema.validate(req.body);
+
+  if (error) return res.status(400).send(error.details[0].message);
+
+  //check if user already exit
+  let user = await User.findOne({ email: req.body.email });
+
+  if (user) return res.status(400).send("User already exist..");
+  //else create a new user using our model
+  user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  });
+
+  const salt = await bcrypt.genSalt(10);
+
+  user.password = await bcrypt.hash(user.password, salt);
+  await user.save();
+
+  const token = genAuthToken(user);
+  res.send(token);
+});
+
+module.exports = router;
